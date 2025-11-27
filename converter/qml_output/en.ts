d259781300451922854ebe7564cbcce8882f7991
tdkhanh <tdkhanh@tma.com.vn>
2025-11-27 16:38:45 +0700

    addd

---
+++ converter/qml_output/en.ts
@@ -0,0 +1,17 @@
+<TS version="2.1" language="en">
+  <context>
+    <name>app</name>
+    <message>
+      <source>hello</source>
+      <translation>Hello</translation>
+    </message>
+    <message>
+      <source>welcome_user</source>
+      <translation>Welcome, %1</translation>
+    </message>
+    <message>
+      <source>call_end</source>
+      <translation>Call ended</translation>
+    </message>
+  </context>
+</TS>
