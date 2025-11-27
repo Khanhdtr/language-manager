d76d66d9533cccc888bf5cfe63d24121d61531be
Khanhdtr <khanhdtr93@github.com>
2025-11-27 10:36:53 +0000

    Auto generate language files

---
+++ converter/qml_output/en.ts
@@ -0,0 +1,65 @@
+<TS version="2.1" language="en">
+  <context>
+    <name>app</name>
+    <message>
+      <source>hello</source>
+      <translation>Hello</translation>
+    </message>
+    <message>
+      <source>welcome_user</source>
+      <translation>Welcome, %1</translation>
+    </message>
+    <message>
+      <source>call_end</source>
+      <translation>Call ended</translation>
+    </message>
+    <message>
+      <source>call_start</source>
+      <translation>Call start</translation>
+    </message>
+    <message>
+      <source>call_start1</source>
+      <translation>Call start1</translation>
+    </message>
+    <message>
+      <source>call_start2</source>
+      <translation>Call start123123123</translation>
+    </message>
+    <message>
+      <source>call_start3</source>
+      <translation>Call start123123123</translation>
+    </message>
+    <message>
+      <source>call_start4</source>
+      <translation>Call start123123123</translation>
+    </message>
+    <message>
+      <source>call_start5</source>
+      <translation>Call start123123123</translation>
+    </message>
+    <message>
+      <source>call_start6</source>
+      <translation>Call start123123123</translation>
+    </message>
+    <message>
+      <source>call_start7</source>
+      <translation>Call start123123123</translation>
+    </message>
+    <message>
+      <source>call_start8</source>
+      <translation>Call start123123123</translation>
+    </message>
+    <message>
+      <source>call_start9</source>
+      <translation>Call start123123123</translation>
+    </message>
+    <message>
+      <source>call_start10</source>
+      <translation>Call start123123123</translation>
+    </message>
+    <message>
+      <source>call_start11</source>
+      <translation>Call start123123123</translation>
+    </message>
+  </context>
+</TS>
